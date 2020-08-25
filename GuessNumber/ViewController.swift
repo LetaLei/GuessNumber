ab72b2d70a32f5ca859424196c8beb0ac625bbaa
max <max1218@gmail.com>
2020-08-25 21:21:38 +0800

    Initial Commit

---
+++ GuessNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessNumber
+//
+//  Created by max on 2020/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
